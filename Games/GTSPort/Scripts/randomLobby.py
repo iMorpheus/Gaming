c589ba2e9745b06bfc23a65b3f708afaccffd853
iMorph <iMorpheus@users.noreply.github.com>
2020-05-11 20:08:38 +0900

    Update randomLobby.py

--- Games/GTSPort/Scripts/randomLobby.py
+++ Games/GTSPort/Scripts/randomLobby.py
@@ -8,7 +8,7 @@ country_set = ['US', 'GB', 'KR', 'IT', 'FR', 'GER', 'JP']
 class_set = ['N100', 'N200', 'N300', 'N400', 'N500', 'N600', 'N700', 'N800', 'N900', 'N1000', 'GrB', 'Gr4', 'Gr3', 'Gr2', 'Gr1', 'GrX']
 tyre_set = ['CH', 'SH', 'RH']
 damage_set = ['Light', 'Strong']
-boost_set = ['Weak' 'Strong']
+boost_set = ['Weak', 'Strong']
 
 
 # RANDOMIZE
