2aef42bf57c6ed3c4e711d01fe0b08190cd3df0d
iMorph <iMorpheus@users.noreply.github.com>
2020-05-18 14:48:12 +0900

    Update randomLobby.py
    Changed tyre options to make races more enjoyable.
    Removed Gr. B. Will be added later in a category for oddball races/one-makes

--- Games/GTSPort/Scripts/randomLobby.py
+++ Games/GTSPort/Scripts/randomLobby.py
@@ -1,21 +1,22 @@
+
 #!/usr/local/bin/Python3
 
 import random
 
 # -- INPUTS/VAR -- #
 n_set = ['N100', 'N200', 'N300', 'N400', 'N500', 'N600', 'N700', 'N800', 'N900', 'N1000']
-gr_set = ['GrB', 'Gr4', 'Gr3', 'Gr2', 'Gr1', 'GrX']
-hard_tyre_set = ['CH', 'SH', 'RH']
-soft_tyre_set = ['CS', 'SS', 'RS']
-damage_set = ['Light', 'Strong']
+gr_set = ['Gr4', 'Gr3', 'Gr2', 'Gr1', 'GrX']
+slow_tyre_set = ['CM', 'SM', 'RH']
+fast_tyre_set = ['CS', 'SS', 'RM', 'RS']
+damage_set = ['ON', 'OFF']
 boost_set = ['Weak', 'Strong']
 
 
 # -- SORTING HATS -- #
 n_class = random.choice(n_set)
 gr_class = random.choice(gr_set)
-hard_tyres = random.choice(hard_tyre_set)
-soft_tyres = random.choice(soft_tyre_set)
+slow_tyres = random.choice(slow_tyre_set)
+fast_tyres = random.choice(fast_tyre_set)
 damage_level = random.choice(damage_set)
 boost_level = random.choice(boost_set)
 
@@ -31,15 +32,15 @@ else:
     car_class = (gr_class)
 
 # TYRES:
-compound_set = ['H', 'S']
+compound_set = ['SLOW', 'FAST']
 compound = random.choice(compound_set)
 
-if compound == 'H':
-    tyres = (hard_tyres)
+if compound == 'SLOW':
+    tyres = (slow_tyres)
 else:
-    tyres = (soft_tyres)
+    tyres = (fast_tyres)
 
 # -- OUTPUT -- #
-print(f"Let's race {car_class} cars on {tyres} with {damage_level} DAMAGE and BOOST set to {boost_level}")
+print(f"Let's race {car_class}s on {tyres} with DAMAGE:[{damage_level}] and BOOST set to {boost_level}")
 
 # https://pynative.com/python-random-choice/
